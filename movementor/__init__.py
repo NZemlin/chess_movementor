b48ff863e44ae1dd73ae859cdccd9446ce5d3ff5
Nicholas Zemlin <nicholaszemlin@gmail.com>
2024-01-31 21:06:06 -0800

    Added database initialization for deployment

--- movementor/__init__.py
+++ movementor/__init__.py
@@ -22,6 +22,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    from db import init_db
+    init_db()
     
     from . import db
     db.init_app(app)
